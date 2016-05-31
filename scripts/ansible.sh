6764afcb5888ac193dd59accd404b6ca6f9247ed
Jeff Geerling <geerlingguy@mac.com>
2016-05-30 22:43:19 -0500

    Update Ansible installation since PPA works in 16.04.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,20 +1,10 @@
 #!/bin/bash -eux
 
-# Install pip.
-apt -y install python-setuptools python-dev libffi-dev
-easy_install pip
-
-# Install Ansible.
-pip install ansible
-
-# TODO: Switch to official apt repo after the following issue is resolved:
-# https://github.com/ansible/ansible/issues/15524
-
 # Install Ansible repository.
-# apt -y update && apt-get -y upgrade
-# apt -y install software-properties-common
-# apt-add-repository ppa:ansible/ansible
+apt -y update && apt-get -y upgrade
+apt -y install software-properties-common
+apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
-# apt -y update
-# apt -y install ansible
+apt -y update
+apt -y install ansible
