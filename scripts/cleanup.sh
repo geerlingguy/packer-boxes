9455d865a13490e03d7779aee5c04a23d43ec3e5
Jeff Geerling <geerlingguy@mac.com>
2016-10-27 21:40:42 -0500

    Fixes #11, and others: Order of install and cleanup cleanup.sh.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,22 +1,8 @@
 #!/bin/bash -eux
 
-# Work around bug with Vagrant's handling of stuff.
-echo 'GRUB_CMDLINE_LINUX="biosdevname=0 net.ifnames=0"' >> /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo 'auto eth0' >> /etc/network/interfaces
-echo 'iface eth0 inet dhcp' >> /etc/network/interfaces
-echo 'auto eth1' >> /etc/network/interfaces
-echo 'iface eth1 inet manual' >> /etc/network/interfaces
-
 # Uninstall Ansible and remove PPA.
-# apt -y remove --purge ansible
-# apt-add-repository --remove ppa:ansible/ansible
-
-# Uninstall Ansible and other Pip deps.
-pip freeze | grep -v "^-e" | xargs pip uninstall -y
-easy_install -m pip
-apt -y purge --auto-remove python-setuptools python-dev
+apt -y remove --purge ansible
+apt-add-repository --remove ppa:ansible/ansible
 
 # Apt cleanup.
 apt autoremove
