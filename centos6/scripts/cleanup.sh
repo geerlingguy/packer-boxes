3a9e21a1485e03c70062e4d6dd0abb58a9ca9a0b
Jeff Geerling <geerlingguy@mac.com>
2020-02-10 10:44:57 -0600

    Fixes #49: Use non-local Ansible provisioner on CentOS 6 because of SSL issues.

--- centos6/scripts/cleanup.sh
+++ centos6/scripts/cleanup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -eux
 
-# Remove Ansible and its dependencies.
-yum -y remove git python-setuptools gcc python-devel openssl-devel
-
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
