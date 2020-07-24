1f2d747ac83c0ca5288ff8a1377ee7d1a13ce5ba
nedster1980 <nedster1980@gmail.com>
2020-07-24 09:13:03 +0100

    Added version 0.1.1 node version needs another look

--- ubuntu2004/scripts/software.sh
+++ ubuntu2004/scripts/software.sh
@@ -17,15 +17,27 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 #Add GPG Key for Ansible
 curl -fsSL http://ppa.launchpad.net/ansible/ansible/ubuntu/dists/bionic/Release.gpg | apt-key add -
 
+#Add nodejs
+curl -fsSL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+nodesource_setup.sh
+
+
 #Add Docker repo to apt sources
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 #Add K8s to apt sources will need to be updated to focal fossa when it becomes available
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
+#Add .NET Core SDK see https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#2004-
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+
 #Update repo for Docker
 apt-get -y update
 
-#Install Docker, Ansible and K8s 
+#Install Docker, Ansible, K8s, dotnet core 3.1 and Open JDK 8
 apt-get -y install docker-ce
 apt-get -y install kubelet kubeadm kubectl kubernetes-cni
+apt-get -y install dotnet-sdk-3.1
+apt-get -y install openjdk-11-jre-headless
+apt-get -y install nodejs
