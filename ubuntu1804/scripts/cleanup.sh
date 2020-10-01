c81e6e5ed0200fd777901fd4b324febefdfc2311
Oskar Schöldström <m@oxy.fi>
2020-10-01 14:09:16 -0300

    Fix identical machine-ids causing dhcp servers to to use same IPs

--- ubuntu1804/scripts/cleanup.sh
+++ ubuntu1804/scripts/cleanup.sh
@@ -8,6 +8,11 @@ apt-add-repository --remove ppa:ansible/ansible
 apt autoremove
 apt update
 
+#  Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
+truncate -s 0 /etc/machine-id
+rm /var/lib/dbus/machine-id
+ln -s /etc/machine-id /var/lib/dbus/machine-id
+
 # Delete unneeded files.
 rm -f /home/vagrant/*.sh
 
